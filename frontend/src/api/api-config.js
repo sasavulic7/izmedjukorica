f2b1121c376fbea685d8f1821c554d437814828e
sasavulic7 <62356252+sasavulic7@users.noreply.github.com>
2025-03-15 00:40:26 +0100

    Update api-config.js

--- frontend/src/api/api-config.js
+++ frontend/src/api/api-config.js
@@ -1,22 +1,20 @@
 import axios from "axios";
 
-// Kreiranje instance axios-a sa baseURL za API zahteve
-const API_URL = "https://izmedjukorica.vercel.app/api";
-
 const api = axios.create({
-  baseURL: API_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
+    baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
-// Interceptor za dodavanje auth tokena na svaki zahtev
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers["x-auth-token"] = token;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default api;
