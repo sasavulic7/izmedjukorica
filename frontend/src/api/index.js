447123c5efebabe0ad1502d6fc277e0b604789ed
sasavulic7 <62356252+sasavulic7@users.noreply.github.com>
2025-03-14 01:26:32 +0100

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: API_URL
 });
 
 // Interceptor za dodavanje tokena u zahteve
