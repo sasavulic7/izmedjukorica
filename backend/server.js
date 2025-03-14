950b482ff954afa3a4f0f120dd414583c5506477
sasavulic7 <62356252+sasavulic7@users.noreply.github.com>
2025-03-15 00:38:06 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,31 +7,29 @@ dotenv.config();
 
 const app = express();
 
-// Ispravljena CORS konfiguracija
 app.use(
-  cors({
-    origin: "https://izmedjukorica-blog-vte.vercel.app", // Ispravan frontend URL
-    credentials: true
-  })
+    cors({
+        origin: "https://izmedjukorica-blog-vte.vercel.app",
+        credentials: true,
+    })
 );
 
 app.use(express.json());
 
 const authRoutes = require("./routes/authRoutes");
-app.use("/api/auth", authRoutes);
-
 const bookRoutes = require("./routes/bookRoutes");
-app.use("/api/books", bookRoutes);
-
 const contactRoutes = require("./routes/contactRoutes");
-app.use("/api", contactRoutes);
+
+app.use(process.env.API_BASE_URL + "/auth", authRoutes);
+app.use(process.env.API_BASE_URL + "/books", bookRoutes);
+app.use(process.env.API_BASE_URL, contactRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("Connected to MongoDB");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => console.log(err));
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("Connected to MongoDB");
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    })
+    .catch((err) => console.log(err));
