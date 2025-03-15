6c53ccd69eaa61f0396e44cc7a134c6f188395aa
sasavulic7 <62356252+sasavulic7@users.noreply.github.com>
2025-03-15 01:39:21 +0100

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,14 +4,17 @@ module.exports = function (req, res, next) {
     const token = req.header("x-auth-token");
 
     if (!token) {
+        console.log("Nema tokena, pristup odbijen");
         return res.status(401).json({ message: "Nema tokena, pristup odbijen" });
     }
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log("Token verifikovan:", decoded);
         req.userId = decoded.userId;
         next();
     } catch (error) {
+        console.log("Token nije validan:", error.message);
         res.status(401).json({ message: "Token nije validan" });
     }
 };
